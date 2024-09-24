f4c82a9399d4d3f7d72d70e276328e6dc1599629
Allyson_Augusto <131546153+AllysonAugusto@users.noreply.github.com>
2024-09-24 11:32:58 -0300

    Update Mensagem.py

--- SistemasDistribuidos/Mensagem.py
+++ SistemasDistribuidos/Mensagem.py
@@ -1,13 +1,11 @@
 import json 
-from Proxy import Proxy 
-
 
 class Mensagem:
     def __init__(self, json_obj=None):
         if json_obj:
-            self.messageType = json_obj.get("tipoMensagem", None) 
-            self.id_metodo = json_obj.get("methodId", None)        
-            self.argumento = json_obj.get("argument", {})   
+            self.messageType = json_obj.get("tipoMensagem", None)
+            self.id_metodo = json_obj.get("methodId", None)
+            self.argumento = json_obj.get("argumentos", {})
         else:
             print("Erro: JSON não foi definido")
 
@@ -18,19 +16,21 @@ class Mensagem:
             "argumentos": self.argumento,
         })
 
+    @staticmethod
     def para_objeto(json_string):
         json_obj = json.loads(json_string)
         return Mensagem(json_obj)
 
-json_data = '{"tipoMensagem": 1, "methodId": "metodo1", "argumentos": {"arg1": "valor1"}'
+# JSON corrigido
+json_data = '{"tipoMensagem": 1, "methodId": "metodo1", "argumentos": {"arg1": "valor1"}}'
 
-#Json -> objeto
+# Json -> objeto
 mensagem = Mensagem.para_objeto(json_data)
 
-
-print(mensagem.messageType) 
-print(mensagem.id_metodo)     
-print(mensagem.argumento)     
+# Acessando os atributos do objeto
+print(mensagem.messageType)  # Saída: 1
+print(mensagem.id_metodo)    # Saída: metodo1
+print(mensagem.argumento)    # Saída: {'arg1': 'valor1'}
 
 # objeto -> json
-print(mensagem.para_json())
+print(mensagem.para_json())  # Converte de volta para JSON
